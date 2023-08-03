7340776ffd83b4b801f0c5e2e8f734fc233f85b9
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-08-03 18:17:38 -0400

    added email trigger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests 
 import selectorlib
+import os 
+import smtplib, ssl
+import time 
 
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 
@@ -18,8 +21,21 @@ def extract(source):
     value = extractor.extract(source)['tours']
     return value 
 
-def send_email():
-    pass
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = 'emilyphalperin@gmail.com'
+    password = 'enter pw'
+
+    receiver = 'emilyphalperin@gmail.com'
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+    
 
 def store(extracted):
     with open('data.txt', 'a') as file:
@@ -30,15 +46,17 @@ def read(extracted):
         return file.read()
 
 if __name__ == '__main__':
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    
-    content = read(extracted)
-    print(extracted)
-    if extracted != 'No upcoming tours':
-        if extracted not in content:
-            #only store an event when the event is new 
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        
+        content = read(extracted)
+        print(extracted)
+        if extracted != 'No upcoming tours':
+            if extracted not in content:
+                #only store an event when the event is new 
+                store(extracted)
+                send_email(message='Hey, new event was found.')
+        time.sleep(2)
 
 
